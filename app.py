ca32ec9c794615a94bdf0ece4d89e18de91b78ef
carmartinezm <94133435+carmartinezm@users.noreply.github.com>
2021-11-12 16:07:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def hello():
     user = request.args.get('user')
     password = request.args.get('password')
     if (user == 'atlanticomedio') and (password == '1234'):
-        return "BIENVENIDO SEAS A NUESTRA SECTA"
+        return Response( "BIENVENIDO SEAS A NUESTRA SECTA" , status=200)
     else:
-        return "TU ACCESO HA SIDO DENEGADO, LA AUTODESTRUCCIÓN COMENZARÁ EN 3, 2, 1... ¡¡¡¡BOOOOOMMMM!!!"
+        return Response("TU ACCESO HA SIDO DENEGADO, LA AUTODESTRUCCIÓN COMENZARÁ EN 3, 2, 1... ¡¡¡¡BOOOOOMMMM!!!" , status=401)
 
 if __name__ == '__main__':
      app.run(port='5000')
