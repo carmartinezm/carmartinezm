7ced9080063bcf8930b0ebb5b60056c6fc4949c5
carmartinezm <94133435+carmartinezm@users.noreply.github.com>
2021-11-12 15:58:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
@@ -6,7 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Carla Martínez!"
+    user = request.args.get('user')
+    password = request.args.get('password')
+    if (user == 'atlanticomedio') and (password == '1234'):
+        return "BIENVENIDO SEAS A NUESTRA SECTA"
+    else:
+        return "TU ACCESO HA SIDO DENEGADO, LA AUTODESTRUCCIÓN COMENZARÁ EN 3, 2, 1... ¡¡¡¡BOOOOOMMMM!!!"
 
 if __name__ == '__main__':
      app.run(port='5000')
